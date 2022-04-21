9b205c64b73fc8ccb94306fb41c4df256882a19b
Fernando M Torrejón <fmtorrejon@hotmail.com.ar>
2022-04-21 00:56:48 -0300

    clase03 completa, se agrego readTask para que se cargue LocalStorage. Falta solucionar que se recargan los eliminados, es decir no se quitan de localStoree

--- components/addTask.js
+++ components/addTask.js
@@ -2,58 +2,49 @@ import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
 
 export const addTask = (evento) => {
-    const list = document.querySelector('[data-list]');
-    const task = createTask(evento);
-    list.appendChild(task);
-};
-
-
-const createTask = (evento) => {
     evento.preventDefault();
-    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
-    console.log(taskList);
+
+    const list = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
     const calendar = document.querySelector('[data-form-date]');
+
     const value = input.value;
     const date = calendar.value;
     const dateFormat = moment(date).format('DD/MM/YYYY');
-    console.log(dateFormat);
-    const task = document.createElement('li');
-    task.classList.add('card');
+    
     input.value = '';
-    //backticks
-    const taskContent = document.createElement('div');
-    //console.log(value, dateFormat);
+    calendar.value = '';
 
     const taskObj = {
-        //value: value, 
-        //dateFormat: dateFormat
-        //Usaremos el modo simplificado a continuación.
         value,
         dateFormat,
     };
-
+    
+    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
     taskList.push(taskObj);
-
-    //En "sessionStorage" la informaciòn persiste mientras la pestaña del navegador este abierta.
-    //sessionStorage.setItem('tasks', JSON.stringify(taskObj));
-
-    //Para almacenar la información localmente aunque se cierre el navegador usaremos localStorage.
-    // 
     localStorage.setItem('tasks', JSON.stringify(taskList));
 
+    const task = createTask(taskObj);
+    list.appendChild(task);
+};
 
+export const createTask = ({ value, dateFormat }) => {
+    const task = document.createElement('li');
+          task.classList.add('card');
+    
+    const taskContent = document.createElement('div');
+      
     const titleTask = document.createElement('span');
-    titleTask.classList.add('task');
-    titleTask.innerText = value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
-    // task.innerHTML = content;
+          titleTask.classList.add('task');
+          titleTask.innerText = value;
+          taskContent.appendChild(checkComplete());
+          taskContent.appendChild(titleTask);
+    
     const dateElement = document.createElement("span");
-    dateElement.innerHTML = dateFormat;
-    //console.log(dateElement);
-    task.appendChild(taskContent);
-    task.appendChild(dateElement);
-    task.appendChild(deleteIcon());
+          dateElement.innerHTML = dateFormat;
+          
+          task.appendChild(taskContent);
+          task.appendChild(dateElement);
+          task.appendChild(deleteIcon());
     return task;
 };
